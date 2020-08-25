e7beea139c8d41b9645a10d312ead5122cffb776
SadafHaroon2020 <sadafssuet05@gmail.com>
2020-08-25 00:50:19 -0500

    Assignment UserInput Two number Product

---
+++ GetInputFromUserAssignment.java
@@ -0,0 +1,20 @@
+package week1;
+import java.util.Scanner;
+
+public class GetInputFromUserAssignment {
+  public static void main(String args[])
+	    {
+	  Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+	    System.out.println("Enter First Number");
+
+	    int firstNum = myObj.nextInt();  // Read user input
+	      
+	    System.out.println("Enter Second Number");
+	    int secondNum = myObj.nextInt();  // Read user input
+	    System.out.println("First Number is: " + firstNum);
+	    System.out.println("Second Number is: " + secondNum); 
+	    
+	    int productOfTwoNumber = firstNum*secondNum;
+	    System.out.println("Product of two numbers : " +productOfTwoNumber);
+	    }
+}
